f4f0ce37e80b3c82244ae19515a9929dd2609b71
Shoaib <shoaibtasrif326@gmail.com>
2026-07-02 22:02:51 +0600

    [delpoy2]

--- backend/django_settings/wsgi.py
+++ backend/django_settings/wsgi.py
@@ -4,6 +4,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings.settings')
 
 application = get_wsgi_application()
